bb00833e9a4a9f0a1bea69466438f63e32646e39
iastagenie <iastagenie.com>
2026-06-06 09:52:16 +0100

    feat(oski_app_store): lien product.template -> oski.module

---
+++ oski_app_store/models/product_template.py
@@ -0,0 +1,9 @@
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    oski_module_id = fields.One2many(
+        "oski.module", "product_tmpl_id", string="Module store"
+    )
