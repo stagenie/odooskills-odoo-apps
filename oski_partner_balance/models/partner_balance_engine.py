298bb5dc9809682dc5baeb4dd9a0555a6a9fe892
iastagenie <iastagenie.com>
2026-08-09 10:01:34 +0100

    feat(balance): moteur, domaine de base et sections

---
+++ oski_partner_balance/models/partner_balance_engine.py
@@ -0,0 +1,47 @@
+from odoo import api, models
+
+ACCOUNT_TYPES = {
+    'receivable': ['asset_receivable'],
+    'payable': ['liability_payable'],
+    'net': ['asset_receivable', 'liability_payable'],
+}
+
+
+class PartnerBalanceEngine(models.AbstractModel):
+    _name = 'oski.partner.balance.engine'
+    _description = 'Partner Balance Engine'
+
+    @api.model
+    def _sections(self, scope):
+        """Return the list of sections a given scope produces."""
+        if scope == 'both':
+            return ['receivable', 'payable']
+        if scope == 'net':
+            return ['net']
+        return [scope]
+
+    @api.model
+    def _account_types(self, section):
+        return list(ACCOUNT_TYPES[section])
+
+    @api.model
+    def _base_domain(self, options, section):
+        """Domain on account.move.line, without any date boundary."""
+        domain = [
+            ('account_id.account_type', 'in', self._account_types(section)),
+            ('partner_id', '!=', False),
+            ('move_id.oski_exclude_from_balance', '=', False),
+        ]
+        if options.get('target_moves', 'posted') == 'all':
+            domain.append(('parent_state', 'in', ('draft', 'posted')))
+        else:
+            domain.append(('parent_state', '=', 'posted'))
+        if options.get('partner_ids'):
+            domain.append(('partner_id', 'in', list(options['partner_ids'])))
+        journal_ids = list(options.get('journal_ids') or [])
+        journal_filter = options.get('journal_filter', 'all')
+        if journal_ids and journal_filter == 'include':
+            domain.append(('journal_id', 'in', journal_ids))
+        elif journal_ids and journal_filter == 'exclude':
+            domain.append(('journal_id', 'not in', journal_ids))
+        return domain
--- oski_partner_balance/tests/test_partner_balance_engine.py
+++ oski_partner_balance/tests/test_partner_balance_engine.py
@@ -50,3 +50,62 @@ class TestPartnerBalanceEngine(TransactionCase):
         move.oski_operation_datetime = '2026-01-15 08:30:00'
         self.assertEqual(
             str(move.oski_operation_datetime), '2026-01-15 08:30:00')
+
+    def _options(self, **kw):
+        """Default option dict, overridable field by field."""
+        options = {
+            'date_from': '2026-01-01',
+            'date_to': '2026-12-31',
+            'include_opening': True,
+            'partner_ids': [],
+            'scope': 'receivable',
+            'journal_filter': 'all',
+            'journal_ids': [],
+            'target_moves': 'posted',
+        }
+        options.update(kw)
+        return options
+
+    def test_10_sections_per_scope(self):
+        engine = self.env['oski.partner.balance.engine']
+        self.assertEqual(engine._sections('receivable'), ['receivable'])
+        self.assertEqual(engine._sections('payable'), ['payable'])
+        self.assertEqual(engine._sections('both'), ['receivable', 'payable'])
+        self.assertEqual(engine._sections('net'), ['net'])
+
+    def test_11_account_types_per_section(self):
+        engine = self.env['oski.partner.balance.engine']
+        self.assertEqual(engine._account_types('receivable'), ['asset_receivable'])
+        self.assertEqual(engine._account_types('payable'), ['liability_payable'])
+        self.assertEqual(
+            engine._account_types('net'),
+            ['asset_receivable', 'liability_payable'])
+
+    def test_12_base_domain_excludes_flagged_moves(self):
+        engine = self.env['oski.partner.balance.engine']
+        domain = engine._base_domain(self._options(), 'receivable')
+        self.assertIn(('move_id.oski_exclude_from_balance', '=', False), domain)
+        self.assertIn(('parent_state', '=', 'posted'), domain)
+        self.assertIn(('partner_id', '!=', False), domain)
+
+    def test_13_base_domain_target_moves_all(self):
+        engine = self.env['oski.partner.balance.engine']
+        domain = engine._base_domain(self._options(target_moves='all'), 'receivable')
+        self.assertIn(('parent_state', 'in', ('draft', 'posted')), domain)
+
+    def test_14_base_domain_journal_include_and_exclude(self):
+        engine = self.env['oski.partner.balance.engine']
+        jid = self.journal_sale.id
+        included = engine._base_domain(
+            self._options(journal_filter='include', journal_ids=[jid]), 'receivable')
+        self.assertIn(('journal_id', 'in', [jid]), included)
+        excluded = engine._base_domain(
+            self._options(journal_filter='exclude', journal_ids=[jid]), 'receivable')
+        self.assertIn(('journal_id', 'not in', [jid]), excluded)
+
+    def test_15_base_domain_ignores_empty_journal_list(self):
+        """Choosing 'include' without picking a journal must not empty the report."""
+        engine = self.env['oski.partner.balance.engine']
+        domain = engine._base_domain(
+            self._options(journal_filter='include', journal_ids=[]), 'receivable')
+        self.assertFalse([leaf for leaf in domain if leaf[0] == 'journal_id'])
